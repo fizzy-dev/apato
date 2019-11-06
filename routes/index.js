67d40cfc69f09f6ff249d58beea340002e5b65e4
Duong Le <tduongle10@gmail.com>
2019-11-07 02:00:59 +0700

    codebase

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const indexRouter = express.Router();
+
+const userRouter = require('./users');
+
+indexRouter.use('/users', userRouter);
+
+module.exports = indexRouter;
